f2e0758a20564e1aa1506fac80200a4c774b4dc0
M-307 <unturnedmc1@gmail.com>
2021-06-05 23:32:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 import discord
+import pymongo
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="?")
 
-CHANNEL_ID = 1234567890123456
-TOKEN = "ODAxMzY5NTIyOTA4OTU0NjU0.YAfrhg.KILnfSxrqxeEK8_a_9ogkUp6gIY"
+# Setup
+CHANNEL_ID = 839863432374911046
+TOKEN = "ODAxMzY5NTIyOTA4OTU0NjU0.YAfrhg.............."
 PREFIX = "Q:"
+MONGODB_URL = "mongodb://localhost:27017/"
 
-db = []
+# Connect DB
+db = pymongo.MongoClient(MONGODB_URL)
 
+# Ready
 @bot.event
 async def on_ready():
     print("READY!")
 
+# Message
 @bot.event
 async def on_message(msg):
     if msg.channel.id == CHANNEL_ID:
         if msg.reference:
-            for x in db:
-                if x["msgid"] == str(msg.reference.message_id):
-                    x["reply"].append({
-                        "id": str(msg.id),
-                        "msg": msg.content
-                    })
-
-                    print(db)
+            collection = db["discord_reply"]["reply"]
+            if collection.find_one({"msgid": str(msg.reference.message_id)}):
+                print("OK")
+                collection.update_one({
+                    "msgid": str(msg.reference.message_id),
+                },
+                {
+                    "$push": {
+                        "reply": {
+                            "msgid": str(msg.id),
+                            "msg": msg.content
+                        }
+                    }
+                })
                 
         elif msg.content[0:2] == PREFIX:
-            db.append({
-                "msgid": str(msg.id),
-                "msg": msg.content.split(PREFIX + " ")[-1],
-                "reply": []
-            })
-
-            print(db)
+            collection = db["discord_reply"]["reply"]
+            if not collection.find_one({"msgid": str(msg.id)}):
+                collection.insert_one({
+                    "msgid": str(msg.id),
+                    "msg": msg.content.split(PREFIX + " ")[-1],
+                    "reply": []
+                })
 
 bot.run(TOKEN)
